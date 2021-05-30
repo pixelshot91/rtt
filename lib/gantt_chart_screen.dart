1f4573e06fb2912a48d6d42cc96aa216d5f87859
Julien Gautier <julien.gautier@mobile-devices.fr>
2021-05-30 13:48:56 +0200

    Add Transport and color

--- lib/gantt_chart_screen.dart
+++ lib/gantt_chart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:date_utils/date_utils.dart';
 import 'dart:math';
+import 'package:tuple/tuple.dart';
 
 //import 'models.dart';
 
@@ -14,11 +15,43 @@ class Trip {
 
 class Leg {
   int id;
-  String name;
+  Transport transport;
   DateTime startTime;
   DateTime endTime;
 
-  Leg({this.id, this.name, this.startTime, this.endTime});
+  Leg({this.id, this.transport, this.startTime, this.endTime});
+}
+
+enum TransportKind {
+  RER,
+  METRO,
+  BUS,
+}
+//Map colorMap = <Pair<TransportKind, line>, Color>();
+
+Color ColorRGB(int r, int g, int b) => Color.fromARGB(255, r, g, b);
+
+class RATPColors {
+  static final Coquelicot = ColorRGB(255, 20, 0);
+  static final Bleu_outremer = ColorRGB(60, 145, 220);
+  static final Vert_fonce = ColorRGB(0, 100, 60);
+}
+var colorMap = {
+  Tuple2(TransportKind.RER, "A"): RATPColors.Coquelicot,
+  Tuple2(TransportKind.RER, "B"): RATPColors.Bleu_outremer,
+  Tuple2(TransportKind.BUS, "172"): RATPColors.Vert_fonce,
+};
+var transportKindNames = {
+  TransportKind.RER: "RER",
+  TransportKind.METRO: "Metro",
+  TransportKind.BUS: "Bus",
+};
+class Transport {
+  TransportKind kind;
+  String line;
+  Color get color => colorMap[Tuple2(kind, line)];
+  String get name => transportKindNames[kind] + ' ' + line;
+  Transport(this.kind, this.line);
 }
 
 class GranttChartScreen extends StatefulWidget {
@@ -36,8 +69,12 @@ class GranttChartScreenState extends State<GranttChartScreen> with TickerProvide
 
   List<Trip> trips = [
     Trip(name: 'Trip 1', legs: [
-      Leg(id: 1, name: 'Basetax', startTime: DateTime(2020, 1, 1, 10, 0), endTime: DateTime(2020, 1, 1, 10, 25)),
-      Leg(id: 3, name: 'Uber', startTime: DateTime(2020, 1, 1, 10, 30), endTime: DateTime(2020, 1, 1, 10, 40)),
+      Leg(id: 1, transport: Transport(TransportKind.RER, "A"), startTime: DateTime(2020, 1, 1, 10, 0), endTime: DateTime(2020, 1, 1, 10, 25)),
+      Leg(id: 3, transport: Transport(TransportKind.RER, "B"), startTime: DateTime(2020, 1, 1, 10, 30), endTime: DateTime(2020, 1, 1, 10, 40)),
+    ]),
+    Trip(name: 'Trip 2', legs: [
+      Leg(id: 1, transport: Transport(TransportKind.BUS, "172"), startTime: DateTime(2020, 1, 1, 10, 11), endTime: DateTime(2020, 1, 1, 10, 32)),
+      Leg(id: 3, transport: Transport(TransportKind.RER, "B"), startTime: DateTime(2020, 1, 1, 10, 40), endTime: DateTime(2020, 1, 1, 10, 50)),
     ]),
   ];
 
@@ -114,11 +151,6 @@ class GanttChart extends StatelessWidget {
     return DateTime(year, month);
   }
 
-  Color randomColorGenerator() {
-    var r = new Random();
-    return Color.fromRGBO(r.nextInt(256), r.nextInt(256), r.nextInt(256), 0.75);
-  }
-
   int calculateNumberOfMinutesBetween(DateTime from, DateTime to) {
     //return to.month - from.month + 12 * (to.year - from.year) + 1;
     return to.difference(from).inMinutes;
@@ -157,32 +189,32 @@ class GanttChart extends StatelessWidget {
   }
 
   List<Widget> buildChartBars(
-      List<Leg> data, double chartViewWidth, Color color) {
+      List<Leg> legs, double chartViewWidth) {
     List<Widget> chartBars = [];
 
-    for(int i = 0; i < data.length; i++) {
+    for(int i = 0; i < legs.length; i++) {
       var remainingWidth =
-      calculateRemainingWidth(data[i].startTime, data[i].endTime);
+      calculateRemainingWidth(legs[i].startTime, legs[i].endTime);
       print(remainingWidth);
       if (remainingWidth > 0) {
         chartBars.add(Container(
           decoration: BoxDecoration(
-              color: color.withAlpha(100),
+              color: legs[i].transport.color,
               borderRadius: BorderRadius.circular(10.0)),
           height: 25.0,
           width: remainingWidth * chartViewWidth / viewRangeToFitScreen,
           margin: EdgeInsets.only(
-              left: calculateDistanceToLeftBorder(data[i].startTime) *
+              left: calculateDistanceToLeftBorder(legs[i].startTime) *
                   chartViewWidth /
                   viewRangeToFitScreen,
               top: i == 0 ? 4.0 : 2.0,
-              bottom: i == data.length - 1 ? 4.0 : 2.0
+              bottom: i == legs.length - 1 ? 4.0 : 2.0
           ),
           alignment: Alignment.centerLeft,
           child: Padding(
             padding: const EdgeInsets.only(left: 8.0),
             child: Text(
-              data[i].name,
+              legs[i].transport.name,
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
               style: TextStyle(fontSize: 10.0),
@@ -201,9 +233,9 @@ class GanttChart extends StatelessWidget {
     DateTime tempDate = fromDate;
 
     headerItems.add(Container(
-      width: chartViewWidth / viewRangeToFitScreen,
+      width: 3*chartViewWidth / viewRangeToFitScreen,
       child: new Text(
-        'NAME',
+        ' ',
         textAlign: TextAlign.center,
         style: TextStyle(
           fontSize: 10.0,
@@ -213,21 +245,21 @@ class GanttChart extends StatelessWidget {
 
     for (int i = 0; i < viewRange; i++) {
       headerItems.add(Container(
-        width: chartViewWidth / viewRangeToFitScreen,
+        width: 5 * chartViewWidth / viewRangeToFitScreen,
         child: new Text(
-          " ", //tempDate.month.toString() + '/' + tempDate.year.toString(),
-          textAlign: TextAlign.center,
+          tempDate.hour.toString() + ':' + tempDate.minute.toString(),
+          textAlign: TextAlign.left,
           style: TextStyle(
             fontSize: 10.0,
           ),
         ),
       ));
-      tempDate = nextMonth(tempDate);
+      tempDate = tempDate.add(Duration(minutes: 5));
     }
 
     return Container(
       height: 25.0,
-      color: color.withAlpha(100),
+      color: color,
       child: Row(
         children: headerItems,
       ),
@@ -254,8 +286,8 @@ class GanttChart extends StatelessWidget {
   }
 
   Widget buildChartForEachTrip(Trip trip, double chartViewWidth) {
-    Color color = randomColorGenerator();
-    var chartBars = buildChartBars(trip.legs, chartViewWidth, color);
+    final color = ColorRGB(200, 200, 200);
+    var chartBars = buildChartBars(trip.legs, chartViewWidth);
     return Container(
       height: chartBars.length * 29.0 + 25.0 + 4.0,
       child: ListView(
@@ -328,26 +360,3 @@ class GanttChart extends StatelessWidget {
     );
   }
 }
-
-
-/*var projects = [
-  Leg(id: 1, name: 'Basetax', startTime: DateTime(2020, 1, 1, 10, 0), endTime: DateTime(2020, 1, 1, 10, 25)
-  ),
-  /*Project(id: 2, name: 'CENTTO', startTime: DateTime(2018, 4, 1), endTime: DateTime(2018, 6, 1),
-      participants: [2, 3]
-  ),*/
-  Leg(id: 3, name: 'Uber', startTime: DateTime(2020, 1, 1, 10, 30), endTime: DateTime(2020, 1, 1, 10, 40)
-  ),
-  /*Project(id: 4, name: 'Grab', startTime: DateTime(2018, 6, 1), endTime: DateTime(2018, 10, 1),
-      participants: [1, 4, 3]
-  ),
-  Project(id: 5, name: 'GO-JEK', startTime: DateTime(2017, 3, 1), endTime: DateTime(2018, 11, 1),
-      participants: [4, 2, 3]
-  ),
-  Project(id: 6, name: 'Lyft', startTime: DateTime(2018, 4, 1), endTime: DateTime(2018, 7, 1),
-      participants: [4, 2, 3]
-  ),
-  Project(id: 7, name: 'San Jose', startTime: DateTime(2018, 5, 1), endTime: DateTime(2018, 12, 1),
-      participants: [1, 2, 4]
-  ),*/
-];*/
